592a8bef8dda147233ec0dec46ef15141d69dd3a
Jules Lagadic <jules.lagadic@gmail.com>
2024-03-06 01:49:43 +0100

    feat: add webpack and bootstrap

---
+++ webpack.renderer.config.js
@@ -0,0 +1,35 @@
+const rules = require('./webpack.rules');
+const webpack = require('webpack');
+
+rules.push({
+  test: /\.scss$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' },{
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        plugins: function () {
+          return [
+            require('autoprefixer')
+          ];
+        }
+      }
+    }
+  }],
+},{
+  test: /\.(png|jpe?g|gif|ico|svg)$/, // We will handle of these file extensions
+  use: [{ loader: "file-loader" }]
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
+
+};
